de9569e654d2c470f083bd3b3932faf4f800585c
Arex360 <49303647+Arex360@users.noreply.github.com>
2021-09-02 16:58:57 +0500

    added rotation system

--- Assets/Scripts/playerFollow.cs
+++ Assets/Scripts/playerFollow.cs
@@ -7,6 +7,7 @@ public class playerFollow : MonoBehaviour
     [SerializeField] private Transform player;
     [SerializeField] private float speed;
     [SerializeField] private float far;
+    [SerializeField] private List<Transform> constraint;
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
@@ -15,7 +16,8 @@ public class playerFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 position = new Vector3(this.transform.position.x, this.transform.position.y, player.transform.position.z);
+        Vector3 position = new Vector3(player.transform.position.x, this.transform.position.y, player.transform.position.z);
+        position.x = Mathf.Clamp(position.x, constraint[0].position.x, constraint[1].position.x);
         position.z += far;
         this.transform.position = position;
     }
