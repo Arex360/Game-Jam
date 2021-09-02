65f1a4e5aeb039002f094853b616f6ef3a49ddaf
Arex360 <49303647+Arex360@users.noreply.github.com>
2021-09-02 16:12:21 +0500

    new follow system

--- Assets/playerFollow.cs
+++ Assets/playerFollow.cs
@@ -6,7 +6,7 @@ public class playerFollow : MonoBehaviour
 {
     [SerializeField] private Transform player;
     [SerializeField] private float speed;
-    [SerializeField] private Vector3 offset;
+    [SerializeField] private float far;
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
@@ -15,7 +15,8 @@ public class playerFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 position = new Vector3(this.transform.position.x, this.transform.position.y, player.transform.position.z) + offset;
+        Vector3 position = new Vector3(this.transform.position.x, this.transform.position.y, player.transform.position.z);
+        position.z += far;
         this.transform.position = position;
     }
 }
